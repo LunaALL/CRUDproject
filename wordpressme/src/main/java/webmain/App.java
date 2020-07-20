f1143377adb58988599289a101058280e21f1534
Lunall <wooju407@naver.com>
2020-07-20 21:03:49 +0900

    dd

---
+++ wordpressme/src/main/java/webmain/App.java
@@ -0,0 +1,13 @@
+package webmain;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
