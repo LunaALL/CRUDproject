a16aaaf1db9e598dc1adcdc4c1b70594f774927d
Lunall <wooju407@naver.com>
2020-07-21 20:16:26 +0900

    dd

---
+++ wordpressme/src/main/java/controller/HelloController.java
@@ -0,0 +1,17 @@
+package controller;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloController {
+	
+	@GetMapping("/Loginform")
+	public String hello(Model model, String name) {
+		
+		return "Loginform";
+	}
+}
---
+++ wordpressme/src/main/java/member/RegisterUser.java
@@ -0,0 +1,11 @@
+package member;
+
+public class RegisterUser {
+	String id;
+	String email;
+	String name;
+	String subname;
+	String password;
+	
+
+}
