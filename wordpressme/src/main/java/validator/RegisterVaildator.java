016d78f886a7546e5c3e55076aae696e09e7b98b
Lunall <wooju407@naver.com>
2020-07-24 20:50:26 +0900

    수정

--- wordpressme/src/main/java/validator/RegisterVaildator.java
+++ wordpressme/src/main/java/validator/RegisterVaildator.java
@@ -14,10 +14,16 @@ public class RegisterVaildator implements Validator {
 	private static final String emailrequestExp = 
 			"^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@" +
 			"[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+	
+	private static final String pwreqestExp="^[A-Za-z[0-9]]{10,20}$"; 
+
+
 
 	private Pattern pattern;
+	private Pattern pattern2;
 	public RegisterVaildator () {
 		pattern= Pattern.compile(emailrequestExp);
+		pattern2= Pattern.compile(pwreqestExp);
 	}
 	
 	@Override
@@ -46,6 +52,14 @@ public class RegisterVaildator implements Validator {
 				errors.rejectValue("confirmpassword", "nomatch");
 				//비밀번호와 비밀번호 확인이 같을경우 true임. 다를경우 false
 			}
+		}
+		
+		if(!req.getPassword().isEmpty()) {
+			Matcher pwmatcher = pattern2.matcher(req.getPassword());
+			if(!pwmatcher.matches()) {
+				errors.rejectValue("password", "bad");
+			}
+			
 		}
 	}
 
