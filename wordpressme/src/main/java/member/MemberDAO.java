8b10e3f36234e894a6e3751970326335bc314e96
Lunall <wooju407@naver.com>
2020-09-07 22:28:26 +0900

    수정

--- wordpressme/src/main/java/member/MemberDAO.java
+++ wordpressme/src/main/java/member/MemberDAO.java
@@ -13,92 +13,85 @@ import org.springframework.jdbc.core.PreparedStatementCreator;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
 
-public class MemberDAO{
+public class MemberDAO {
 
 	private JdbcTemplate jdbcTemplate;
-	
+
 	public MemberDAO(DataSource dataSource) {
-		this.jdbcTemplate= new JdbcTemplate(dataSource);
+		this.jdbcTemplate = new JdbcTemplate(dataSource);
 	}
 
-	
 	public Memberinfo selectByEmail(String email) {
-		List<Memberinfo> results = jdbcTemplate.query(
-				new PreparedStatementCreator() {
-					
-					@Override
-					public PreparedStatement createPreparedStatement(Connection con) throws SQLException {
-						// TODO Auto-generated method stub
-						PreparedStatement pst = con.prepareStatement("select * from member where EMAIL = ?");
-						pst.setString(1, email);
-						return pst;
-					}
-				}, new MemberinfoRowMapper() );	
-				
+		List<Memberinfo> results = jdbcTemplate.query(new PreparedStatementCreator() {
+			@Override
+			public PreparedStatement createPreparedStatement(Connection con) throws SQLException {
+				// TODO Auto-generated method stub
+				PreparedStatement pst = con.prepareStatement("select * from member where EMAIL = ?");
+				pst.setString(1, email);
+				return pst;
+			}
+		}, new MemberinfoRowMapper());
+
 		return results.isEmpty() ? null : results.get(0);
 	}
-	
+
 	public Memberinfo selectByName(String name) {
-		List<Memberinfo> results = jdbcTemplate.query("select * from member where NAME =? ", new MemberinfoRowMapper(), name);
-		
+		List<Memberinfo> results = jdbcTemplate.query("select * from member where NAME =? ", new MemberinfoRowMapper(),
+				name);
+
 		return results.isEmpty() ? null : results.get(0);
-		
+
 	}
-	
+
 	public void insert(Memberinfo member) {
 		KeyHolder keyHolder = new GeneratedKeyHolder();
 		jdbcTemplate.update(new PreparedStatementCreator() {
 			@Override
 			public PreparedStatement createPreparedStatement(Connection con) throws SQLException {
 				// TODO Auto-generated method stub
-				PreparedStatement pstt = con.prepareStatement("insert into member(EMAIL, PASSWORD, NAME, REGDATE)" + 
-				"values(?,?,?,?)", new String[] {"ID"} );
+				PreparedStatement pstt = con.prepareStatement(
+						"insert into member(EMAIL, PASSWORD, NAME, REGDATE)" + "values(?,?,?,?)",
+						new String[] { "ID" });
 				pstt.setString(1, member.getEmail());
 				pstt.setString(2, member.getPassword());
 				pstt.setString(3, member.getName());
 				pstt.setTimestamp(4, Timestamp.valueOf(member.getRegisterDataTime()));
 				return pstt;
 			}
-		},keyHolder);
+		}, keyHolder);
 		Number keyValue = keyHolder.getKey();
-		member.setId(keyValue.longValue() );
-	
-		
-	}  
-	public void update(Memberinfo member) 
-	{
-		jdbcTemplate.update("update member set NAME = ?, PASSWORD= ? where EMAIL=?", member.getName(), member.getPassword(), member.getEmail() );	
+		member.setId(keyValue.longValue());
+
 	}
-	public Collection<Memberinfo> selectAll() 
-	{
-		List<Memberinfo> results = jdbcTemplate.query("select * from member", new MemberinfoRowMapper() );
-	return results;
+
+	public void update(Memberinfo member) {
+		jdbcTemplate.update("update member set NAME = ?, PASSWORD= ? where EMAIL=?", member.getName(),
+				member.getPassword(), member.getEmail());
 	}
-	
+
+	public Collection<Memberinfo> selectAll() {
+		List<Memberinfo> results = jdbcTemplate.query("select * from member", new MemberinfoRowMapper());
+		return results;
+	}
+
 	public int count() {
-		Integer count = jdbcTemplate.queryForObject("select count(*) from member",Integer.class);
+		Integer count = jdbcTemplate.queryForObject("select count(*) from member", Integer.class);
 		return count;
 	}
-	
-	
+
 	public boolean isAdmin(String email) {
-		List<Memberinfo> results= jdbcTemplate.query("select * from member where admin=1 && EMAIL=?",new MemberinfoRowMapper(), email);
-		if(results.isEmpty()) {
+		List<Memberinfo> results = jdbcTemplate.query("select * from member where admin=1 && EMAIL=?",
+				new MemberinfoRowMapper(), email);
+		if (results.isEmpty()) {
 			return false;
 		}
-		
-		return true;
-	}
-	
-	
-	public boolean deleteMem(String email) {
-		jdbcTemplate.update("delete from member where email=?",email);
-		return true;
-	}
-	
-	
 
-	
-	
+		return true;
+	}
+
+	public boolean deleteMem(String email) {
+		jdbcTemplate.update("delete from member where email=?", email);
+		return true;
+	}
 
 }
