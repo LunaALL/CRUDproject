3a4e8c221a113afa66b9d0dc38462a693699f4ff
Lunall <wooju407@naver.com>
2020-09-08 20:31:07 +0900

    댓글 가져오기 구현중

---
+++ wordpressme/src/main/java/edit/ReplyRowMapper.java
@@ -0,0 +1,24 @@
+package edit;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.springframework.jdbc.core.RowMapper;
+
+public class ReplyRowMapper implements RowMapper<ReplyVO>{
+	@Override
+	public ReplyVO mapRow(ResultSet rs, int rowNum) throws SQLException {
+		// TODO Auto-generated method stub
+		ReplyVO rv = new ReplyVO(
+				rs.getInt("boardnum"),
+				rs.getInt("commentnum"),
+				rs.getString("content"),
+				rs.getString("writer"),
+				rs.getTimestamp("bdcdate").toLocalDateTime()
+				);
+		
+		
+		return rv;
+	}
+
+}
---
+++ wordpressme/src/main/java/edit/ReplyVO.java
@@ -0,0 +1,43 @@
+package edit;
+
+import java.time.LocalDateTime;
+
+public class ReplyVO {
+	private int boardnum;
+	private int commentnum;
+	private String content;
+	private String writer;
+	private LocalDateTime bdcdate;
+	
+	public int getBoardnum() {
+		return boardnum;
+	}
+	public void setBoardnum(int boardnum) {
+		this.boardnum = boardnum;
+	}
+	public int getCommentnum() {
+		return commentnum;
+	}
+	public void setCommentnum(int commentnum) {
+		this.commentnum = commentnum;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getWriter() {
+		return writer;
+	}
+	public void setWriter(String writer) {
+		this.writer = writer;
+	}
+	
+	@Override
+	public String toString() {
+		return "ReplyVO [bno=" + boardnum + ", rno=" + commentnum + ", content=" + content + ", writer=" + writer + ", regdate="
+				+ bdcdate + "]";
+	}
+
+}
