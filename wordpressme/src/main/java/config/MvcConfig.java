7cf4b3973e91c79345969d4d3f2eb68bbf50cad8
Lunall <wooju407@naver.com>
2020-11-29 17:26:56 +0900

    수정

--- wordpressme/src/main/java/config/MvcConfig.java
+++ wordpressme/src/main/java/config/MvcConfig.java
@@ -12,50 +12,49 @@ import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import interceptor.AuthCheckInterceptor;
+
 @Configuration
 @EnableWebMvc
 public class MvcConfig implements WebMvcConfigurer {
-	
+
 	@Override
 	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
 		// TODO Auto-generated method stub
 		configurer.enable();
 	}
-	
+
 	@Override
 	public void configureViewResolvers(ViewResolverRegistry registry) {
 		// TODO Auto-generated method stub
 		registry.jsp("/WEB-INF/view/", ".jsp");
 	}
-	
+
 	@Override
 	public void addViewControllers(ViewControllerRegistry registry) {
 		// TODO Auto-generated method stub
 		registry.addViewController("/main").setViewName("member/main");
 		registry.addViewController("/test").setViewName("member/Viewtest");
 	}
-	
+
 	@Bean
 	public MessageSource messageSource() {
-		ReloadableResourceBundleMessageSource ms =new ReloadableResourceBundleMessageSource();
+		ReloadableResourceBundleMessageSource ms = new ReloadableResourceBundleMessageSource();
 		ms.setBasename("classpath:/message/label");
 		ms.setCacheSeconds(60);
 		ms.setDefaultEncoding("UTF-8");
-		//ms.setUseCodeAsDefaultMessage(true);
+		// ms.setUseCodeAsDefaultMessage(true);
 		return ms;
 	}
-	
+
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
 		// TODO Auto-generated method stub
-		registry.addInterceptor(authCheckInterceptor())
-		.addPathPatterns("/member/change");
+		registry.addInterceptor(authCheckInterceptor()).addPathPatterns("/member/change");
 	}
-	
+
 	@Bean
 	public AuthCheckInterceptor authCheckInterceptor() {
 		return new AuthCheckInterceptor();
 	}
-	
 
 }
